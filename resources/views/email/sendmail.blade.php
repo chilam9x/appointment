068c123e7e5e5a6a696eed150776f4cceda47e9e
Chi Lam <raymond.nguyen9x@gmail.com>
2019-08-13 15:37:24 +0700

    fix title

--- resources/views/email/sendmail.blade.php
+++ resources/views/email/sendmail.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>send mail</title>
+    <title>Cancel an appointment</title>
 </head>
 <body>
     {{$e_message}}
